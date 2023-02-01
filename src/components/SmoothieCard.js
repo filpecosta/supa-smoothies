c1f11d7e123314f0dfa5086622dd91f1fe61c87f
Filipe Costa <filpemdc2@gmail.com>
2023-02-01 17:59:56 -0300

    delete button and function

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,7 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import supabase from '../config/supabaseConfigClient'
 
 export default function SmoothieCard({ smoothie }) {
+
+  const navigate = useNavigate()
+
+  async function handleDelete() {
+    const response = window.confirm('Are you sure you want to delete this smoothie?')
+    if (response) {
+      // delete smoothie
+
+      const { data, error } = await supabase
+        .from('smoothies')
+        .delete()
+        .eq('id', smoothie.id)
+        .select()
+
+      if (error) {
+        console.log(error)
+        alert('Could not delete smoothie')
+      }
+
+      if (data) {
+        alert('Smoothie deleted')
+        console.log(data)
+        window.location.reload()
+      }
+    }
+  }
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
@@ -13,6 +40,7 @@ export default function SmoothieCard({ smoothie }) {
         <Link to={'/' + smoothie.id} >
           <i className='material-icons'>edit</i>
         </Link>
+        <i className='material-icons' onClick={handleDelete}>delete</i>
       </div>
     </div>
   )
