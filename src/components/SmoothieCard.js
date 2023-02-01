b828609559f3d36bddfeb03832b03c4e9e20cb57
Filipe Costa <filpemdc2@gmail.com>
2023-02-01 17:27:31 -0300

    update page created

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SmoothieCard({ smoothie }) {
   return (
@@ -7,6 +8,11 @@ export default function SmoothieCard({ smoothie }) {
       <p>{smoothie.method}</p>
       <div className='rating'>
         {smoothie.rating}
+      </div>
+      <div className='buttons'>
+        <Link to={'/' + smoothie.id} >
+          <i className='material-icons'>edit</i>
+        </Link>
       </div>
     </div>
   )
